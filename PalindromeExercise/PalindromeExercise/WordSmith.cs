4e51090cc3c5757d3a312ed4abd747e8794e9743
Miah Harper <harpermiah@yahoo.com>
2022-06-24 15:04:05 -0400

    finished assignment

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace PalindromeExercise;
+
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+
+		public bool IsAPalindrome(string word)
+        {
+			var reversed = "";
+
+			for(var i = word.Length - 1; i >=0; i--)
+            {
+				reversed += word.ToLower()[i];
+            }
+
+			if(reversed == word.ToLower())
+
+			{
+				return true;
+            }
+
+            else
+            {
+				return false;
+            }
+        }
+	}
+
+//another way to do it is refactor- return reversed == word.ToLower(); after the if
+// or using .linq return word.ToLower().SequenceEqual(word.ToLower().Reverse());
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,32 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("level", true)]
+        [InlineData("miah", false)]
+        [InlineData("bye", false)]
+        [InlineData("Racecar", true)]
+
+        public void PalindromeTester(string word, bool expected)
         {
 
+            //arrange
+            WordSmith reverseWords = new WordSmith();
+
+            //act
+
+            var actual = reverseWords.IsAPalindrome(word);
+
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
